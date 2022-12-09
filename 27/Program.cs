e8a84ee1a26f96e55f8974f11ea5bf5b8dd19453
Kirill_Moiseev_qwertyuiopoiuyhgtfdsdfgh <kilkuruzzza@mail.ru>
2022-12-09 15:40:44 +0300

    s

---
+++ 27/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает 
+//на вход число N и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int a = 1235;
+Console.WriteLine($"Суммф цифр = {GetSumNums(num)}");
+Console.WriteLine($"Суммф цифр = {GetSumNums(a)}");
+
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+}
---
+++ 31/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Double[] array = GetArray(12, -9.0, 9.0);
+Console.WriteLine(String.Join(" ", array));
+
+Double positiveSum = 0;
+Double negativeSum = 0;
+
+foreach (Double el in array)
+{
+    positiveSum += el > 0 ? el : 0;
+} 
+//  for (int i = 0;i < array.Length; i++){
+//     int el = array[i];
+//     positiveSum += el > 0 ? el : 0;
+//     negativeSum += el < 0 ? el : 0;
+// } 
+Console.WriteLine($"Positive sum = {positiveSum}, negative sum = {negativeSum} ");
+
+Double[] GetArray(int size, Double minValue, Double maxValue)
+{
+    Double[] res = new Double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble() * 5.0 - 2.0;
+    }
+    return res;
+}
